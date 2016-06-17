1e898f347198866e40f135ba8b60d6c126d6b264
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-06-17 16:18:11 -0400

    add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+# This is a comment so you can read your program later.
+# Anythign after the # is ignored by ruby
+
+puts "I could have code like this" # and comment after it
+
+# You can also use a comment to 'disable' code
+# puts "this will not run"
+
+puts "this will run"
